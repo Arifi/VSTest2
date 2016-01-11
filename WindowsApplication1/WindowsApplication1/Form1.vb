3379cb596393e46fb807e2b7c003f0eb38751df3
Arifi <arifi.armedia@gmail.com>
2016-01-11 15:10:06 +0100

    text box add

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -4,7 +4,7 @@ Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        ClassLibrary1.Class1.Msg("Hello Git !")
+        ClassLibrary1.Class1.Msg(TextBox1.Text)
 
     End Sub
 End Class
